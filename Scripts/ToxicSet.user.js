5bdb5eb929d98faf9b764c9faf38a5dbd41d468d
SaintRepublic <saintrepublic@bk.ru>
2019-08-18 22:03:06 +0300

    Update ToxicSet.user.js

--- Scripts/ToxicSet.user.js
+++ Scripts/ToxicSet.user.js
@@ -35,6 +35,6 @@
     //Modify small shit
     element = menu.getElementsByClassName("left_menu_nav_wrap")[0];
     element.className += " page_block";
-    element.style.padding = "8px 0px 6px 10px";
+    element.style.padding = "8px 0px 10px 10px";
     //===============================================
 })();
